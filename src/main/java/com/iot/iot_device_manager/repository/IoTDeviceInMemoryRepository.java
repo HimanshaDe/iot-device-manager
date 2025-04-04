e667ceaaaf04cb0163336db23f06d4cd707745aa
Himansha Nawarathna <102845827+HimanshaDe@users.noreply.github.com>
2025-04-04 16:21:54 +0530

    IoT Device data repository implementation

--- src/main/java/com/iot/iot_device_manager/repository/IoTDeviceInMemoryRepository.java
+++ src/main/java/com/iot/iot_device_manager/repository/IoTDeviceInMemoryRepository.java
@@ -1,4 +1,32 @@
 package com.iot.iot_device_manager.repository;
 
+import com.iot.iot_device_manager.model.IoTDevice;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+@Component
 public class IoTDeviceInMemoryRepository {
+    private final Map<Integer, IoTDevice> deviceMap = new ConcurrentHashMap<>();
+
+    public List<IoTDevice> findAll(){
+        return new ArrayList<>(deviceMap.values());
+    }
+
+    public IoTDevice findById(Integer id){
+        return deviceMap.get(id);
+    }
+
+    public void save(IoTDevice ioTDevice){
+        deviceMap.put(ioTDevice.getId(), ioTDevice);
+    }
+
+    public void delete(String id){
+        deviceMap.remove(id);
+    }
+
+    public boolean existsById(Integer id){
+        return deviceMap.containsKey(id);
+    }
+
 }
