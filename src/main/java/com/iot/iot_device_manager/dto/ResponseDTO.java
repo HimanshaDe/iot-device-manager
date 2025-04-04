179fe00acd978115ac3b2ff16e6220cbaadb28e1
Himansha Nawarathna <102845827+HimanshaDe@users.noreply.github.com>
2025-04-04 14:27:31 +0530

    Modal class and DTO class creation

--- src/main/java/com/iot/iot_device_manager/dto/ResponseDTO.java
+++ src/main/java/com/iot/iot_device_manager/dto/ResponseDTO.java
@@ -1,5 +1,6 @@
 package com.iot.iot_device_manager.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,11 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseDTO {
-
+    private int status;
+    private Object data;
+    private String message;
+    private LocalDateTime timestamp;
 }
--- src/main/java/com/iot/iot_device_manager/model/IoTDevice.java
+++ src/main/java/com/iot/iot_device_manager/model/IoTDevice.java
@@ -1,4 +1,16 @@
 package com.iot.iot_device_manager.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class IoTDevice {
+    private int id;
+    private String name;
+    private String type;
+    private Boolean status;
+    private LocalDateTime lastCommunication;
 }
