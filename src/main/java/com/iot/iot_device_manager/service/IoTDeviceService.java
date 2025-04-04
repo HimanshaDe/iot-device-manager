f9a385d6f4b6fa757a0822df9f4d92c906776b3d
Himansha Nawarathna <102845827+HimanshaDe@users.noreply.github.com>
2025-04-04 16:42:00 +0530

    get device by id API implementation

--- src/main/java/com/iot/iot_device_manager/service/IoTDeviceService.java
+++ src/main/java/com/iot/iot_device_manager/service/IoTDeviceService.java
@@ -1,4 +1,7 @@
 package com.iot.iot_device_manager.service;
 
+import com.iot.iot_device_manager.dto.ResponseDTO;
+
 public interface IoTDeviceService {
+    ResponseDTO getDeviceById(Integer id);
 }
--- src/main/java/com/iot/iot_device_manager/service/implementation/IoTDeviceImpl.java
+++ src/main/java/com/iot/iot_device_manager/service/implementation/IoTDeviceImpl.java
@@ -1,4 +1,59 @@
 package com.iot.iot_device_manager.service.implementation;
 
-public class IoTDeviceImpl {
+import com.iot.iot_device_manager.dto.ResponseDTO;
+import com.iot.iot_device_manager.model.IoTDevice;
+import com.iot.iot_device_manager.repository.IoTDeviceInMemoryRepository;
+import com.iot.iot_device_manager.service.IoTDeviceService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.time.LocalDateTime;
+
+@Service
+@Slf4j
+public class IoTDeviceImpl implements IoTDeviceService {
+
+    @Autowired
+    private IoTDeviceInMemoryRepository ioTDeviceInMemoryRepository;
+
+    /*
+     * Api to get a device data by id
+     * @param - device id
+     * @return responseDTO
+     * */
+    @Override
+    public ResponseDTO getDeviceById(Integer id) {
+        ResponseDTO responseDTO = new ResponseDTO();
+
+     try{
+         IoTDevice ioTDevice = ioTDeviceInMemoryRepository.findById(id);
+         if(ioTDevice == null){
+             responseDTO.setMessage("Iot device not found");
+             responseDTO.setStatus(HttpStatus.NOT_FOUND.value());
+         }else {
+             responseDTO.setData(ioTDevice);
+             responseDTO.setMessage("Iot device found");
+             responseDTO.setStatus(HttpStatus.NOT_FOUND.value());
+         }
+         responseDTO.setTimestamp(LocalDateTime.now());
+         return responseDTO;
+
+     }catch (MethodArgumentTypeMismatchException e){
+         log.error("Error occurred :" + "Device id invalid");
+         responseDTO.setMessage("Error occurred :" + "Device id invalid");
+         responseDTO.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+         responseDTO.setTimestamp(LocalDateTime.now());
+         return responseDTO;
+
+     }catch (Exception e){
+         log.error("Error occurred ");
+         responseDTO.setMessage(e.getMessage());
+         responseDTO.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+         responseDTO.setTimestamp(LocalDateTime.now());
+         return responseDTO;
+     }
+    }
 }
