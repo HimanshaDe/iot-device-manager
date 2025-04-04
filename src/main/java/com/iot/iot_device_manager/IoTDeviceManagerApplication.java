3e60bf4ab1b7a1697658b1677fbdbca3f66e0429
Himansha Nawarathna <102845827+HimanshaDe@users.noreply.github.com>
2025-04-04 13:44:45 +0530

    Initial commit

---
+++ src/main/java/com/iot/iot_device_manager/IoTDeviceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.iot.iot_device_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDeviceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTDeviceManagerApplication.class, args);
+	}
+
+}
